ea34366d60559db5bbcbc7bb6067149b7acfc9e4
mordyit <abdullahxit@gmail.com>
2016-03-02 19:43:36 +0300

    FTP_Uploader
    Upload file to FTP server

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txt_file_path = new System.Windows.Forms.TextBox();
+            this.txt_ftp_pass = new System.Windows.Forms.TextBox();
+            this.txt_ftp_user = new System.Windows.Forms.TextBox();
+            this.txt_ftp_server = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.txt_file_path);
+            this.groupBox1.Controls.Add(this.txt_ftp_pass);
+            this.groupBox1.Controls.Add(this.txt_ftp_user);
+            this.groupBox1.Controls.Add(this.txt_ftp_server);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(387, 198);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Upload";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(127, 35);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "Upload";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(306, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // txt_file_path
+            // 
+            this.txt_file_path.Location = new System.Drawing.Point(94, 122);
+            this.txt_file_path.Name = "txt_file_path";
+            this.txt_file_path.ReadOnly = true;
+            this.txt_file_path.Size = new System.Drawing.Size(205, 20);
+            this.txt_file_path.TabIndex = 11;
+            // 
+            // txt_ftp_pass
+            // 
+            this.txt_ftp_pass.Location = new System.Drawing.Point(94, 92);
+            this.txt_ftp_pass.Name = "txt_ftp_pass";
+            this.txt_ftp_pass.Size = new System.Drawing.Size(205, 20);
+            this.txt_ftp_pass.TabIndex = 12;
+            // 
+            // txt_ftp_user
+            // 
+            this.txt_ftp_user.Location = new System.Drawing.Point(94, 64);
+            this.txt_ftp_user.Name = "txt_ftp_user";
+            this.txt_ftp_user.Size = new System.Drawing.Size(205, 20);
+            this.txt_ftp_user.TabIndex = 13;
+            // 
+            // txt_ftp_server
+            // 
+            this.txt_ftp_server.Location = new System.Drawing.Point(94, 36);
+            this.txt_ftp_server.Name = "txt_ftp_server";
+            this.txt_ftp_server.Size = new System.Drawing.Size(205, 20);
+            this.txt_ftp_server.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 125);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "FilePath";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Password";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Username";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "FTP Server";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 217);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FTP_Uploader";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txt_file_path;
+        private System.Windows.Forms.TextBox txt_ftp_pass;
+        private System.Windows.Forms.TextBox txt_ftp_user;
+        private System.Windows.Forms.TextBox txt_ftp_server;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+
+namespace WindowsFormsApplication6
+{
+    public partial class Form1 : Form
+    {
+        public string filepath = null;
+        public string filename = null;
+        public string filePath_down = null;
+        public string fileName_down = null;
+        public WebClient wc = new WebClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            // browas for upload file
+            OpenFileDialog open = new OpenFileDialog();
+            open.InitialDirectory = "C:\\Users\\" + Environment.UserName;
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                filepath = open.FileName;
+                filename = open.SafeFileName;
+            }
+            txt_file_path.Text = filepath;
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            //  upload file
+            WebClient wc = new WebClient();
+            wc.Credentials = new NetworkCredential(txt_ftp_user.Text, txt_ftp_pass.Text);
+            wc.UploadFile(txt_ftp_server.Text + "/" + filename, filepath);
+        }
+
+    }
+}
